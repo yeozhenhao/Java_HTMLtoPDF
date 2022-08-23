92143111b9cab1e0db59eab3b643187c41ed583f
yeozhenhao <elgeneyeo@hotmail.com>
2022-08-23 19:13:37 +0800

    saving work to continue on desktop

--- PDFReader.java
+++ PDFReader.java
@@ -1,24 +1,32 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
 
-import sun.net.www.URLConnection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element; // Note that Element ?may also be imported from the iTextPDF module
 
 public class PDFReader {
 	public static Document HTML_Jsoup_parse(int url_index) {
 		URL url = new URL( "https://medicine.nus.edu.sg/edutech/masteringpsychiatry_200108/#/reader/chapter/"
-	+ (String) url_index);
+	+ Integer.toString(url_index));
 		
-		URLConnection connection = url.openConnection();
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection(); // cast URLConnection to HttpURLConnection - standard way
+		InputStream inputHTML = connection.getInputStream();
         try {
-		    URL url = new URL("http://example.com/file.pdf");
 	        connection = (HttpURLConnection) url.openConnection();
+	        connection.getResponseCode();
 	        connection.connect();
-	        String inputHTML = connection.getInputStream();
 	        // expect HTTP 200 OK, so we don't mistakenly save error report
 	        // instead of the file
 	        if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
-	            return "Server returned HTTP " + connection.getResponseCode() 
-	            + " " + connection.getResponseMessage(); 
+	        	throw new LinkNotAccessible("Server returned HTTP " + connection.getResponseCode() 
+	            + " " + connection.getResponseMessage());
+//	            return "Server returned HTTP " + connection.getResponseCode() 
+//	            + " " + connection.getResponseMessage(); 
 	        }
 	        Document document = Jsoup.parse(inputHTML, "UTF-8");
 	        // document.outputSettings().syntax(Document.OutputSettings.Syntax.xml);
@@ -27,23 +35,28 @@ public class PDFReader {
 	        return e.toString();
         } finally {
 	        try {
-	            if (input != null)
-	                input.close();
+	            if (inputHTML != null)
+	            	inputHTML.close();
 		    } catch (IOException ignored) {
 		    }
-    
             if (connection != null)
             	connection.disconnect();
         	        }
         }
 	
+	public static Element return_Element(String href, Document document) {
+		Element element = document.head()
+		return element;
+	}
+	
 	
 	public static void main(String[] args) {
-		final String outputPdfPath = "E:\LG Documents\JavaProjects\Java_HTMLtoPDF";³
+		final String outputPdfPath = "E:/LG Documents/JavaProjects/Java_HTMLtoPDF";
 		InputStream input = null;
 		OutputStream output = null;
         HttpURLConnection connection = null;
         // Download the file
+        Document document = HTML_Jsoup_parse(1);
         try {
     		output = new FileOutputStream(outputPdfPath);
         } catch (Exception e) {
@@ -58,4 +71,10 @@ public class PDFReader {
         
 	}
 
+}
+
+class LinkNotAccessible extends Exception { 
+    public LinkNotAccessible(String errorMessage) {
+        super(errorMessage);
+    }
 }
