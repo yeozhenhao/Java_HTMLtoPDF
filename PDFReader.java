75c9dc557c04df777f1fa6e2c412954e09e762d9
yeozhenhao <elgeneyeo@hotmail.com>
2022-08-24 00:45:53 +0800

    1. offline_parser tested working

--- PDFReader.java
+++ PDFReader.java
@@ -1,38 +1,113 @@
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.HashMap;
+import java.util.Map;
+import java.io.File;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element; // Note that Element ?may also be imported from the iTextPDF module
+import org.xhtmlrenderer.layout.SharedContext;
+import org.xhtmlrenderer.pdf.ITextRenderer;
 
 public class PDFReader {
-	public static Document HTML_Jsoup_parse(int url_index) {
-		URL url = new URL( "https://medicine.nus.edu.sg/edutech/masteringpsychiatry_200108/#/reader/chapter/"
-	+ Integer.toString(url_index));
+	public static void main(String[] args) {
+		final String inputHTMLPath = "E:/LG Documents/JavaProjects/tempHTMLs/102.html";
+		final String outputPdfPath = "E:/LG Documents/JavaProjects/Java_HTMLtoPDF";
+		final String section_to_output = "section.k-section parsed";
+		final String Pdf_filename = "test.pdf";
+		String base_url = "https://medicine.nus.edu.sg/edutech/masteringpsychiatry_200108/#/reader/chapter/";
+		int page_number = 112;
+		int connection_timeout_millisecond = 3000; 
 		
-		HttpURLConnection connection = (HttpURLConnection) url.openConnection(); // cast URLConnection to HttpURLConnection - standard way
-		InputStream inputHTML = connection.getInputStream();
+//        // Download the file
+//        Document document = HTML_Jsoup_parse(page_number, connection_timeout_millisecond, base_url);
+//        Element element = document_select_section(document, section_to_output);
+//        createPdfFile(element.toString(), outputPdfPath + Pdf_filename);
+		
+		// FOR TESTING ONLY
+		// Offline parsing works but gives <Can't load the XML resource (using TrAX transformer)> error - due to css file not found - tested by removing all lines in text editor with "css" involved.
+		
+		Document document = HTML_offline_parse(inputHTMLPath, base_url); // test with https://stackoverflow.com/questions/12043035/html-to-pdf-using-itext-external-css
+		createPdfFile(document.toString(), outputPdfPath + Pdf_filename);
+	}
+	
+	
+//	private static Document HTML_offline_parse(String fileName, String basefileName) {
+//		Document document = null;
+//		try {
+//	        document = Jsoup.parse(String(fileName), basefileName); 
+//	     // See https://stackoverflow.com/questions/65959750/does-outputsettings-charset-also-change-meta-content-type for the functions below
+//	        document.outputSettings().syntax(Document.OutputSettings.Syntax.xml); // specify that we want XML output // As the next step, we'll use jsoup to convert the above HTML file to a jsoup Document to render XHTML.
+////	        document.charset(StandardCharsets.UTF_8); // update the charset - also adds the <?xml encoding> instruction
+////	        document.select("meta[content~=charset]").remove(); // Remove the obsolete HTML meta tags
+//        } catch (IOException ignored) {
+//        }
+//	        System.out.println("\n---------------Document parsed:---------------\n" + document.toString());
+//        return document;
+//        }
+	
+	private static Document HTML_Jsoup_parse(int url_index, int connection_timeout_millisecond, String base_url) {
+		URL url = null;
+		try {
+			url = new URL( base_url
++ Integer.toString(url_index));
+			System.out.println(url.toString());
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		String url_string = base_url
++ Integer.toString(url_index);
+		Document document = null;
+		InputStream inputHTML = null;
+		HttpURLConnection connection = null;
         try {
-	        connection = (HttpURLConnection) url.openConnection();
-	        connection.getResponseCode();
-	        connection.connect();
-	        // expect HTTP 200 OK, so we don't mistakenly save error report
-	        // instead of the file
-	        if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
-	        	throw new LinkNotAccessible("Server returned HTTP " + connection.getResponseCode() 
-	            + " " + connection.getResponseMessage());
-//	            return "Server returned HTTP " + connection.getResponseCode() 
-//	            + " " + connection.getResponseMessage(); 
-	        }
-	        Document document = Jsoup.parse(inputHTML, "UTF-8");
-	        // document.outputSettings().syntax(Document.OutputSettings.Syntax.xml);
-	        return document;
+        	// TODO uncomment
+//        	connection = (HttpURLConnection) url.openConnection(); // cast URLConnection to HttpURLConnection - standard way
+//			inputHTML = connection.getInputStream();
+//	        connection = (HttpURLConnection) url.openConnection();
+//	        connection.getResponseCode();
+//	        connection.connect();
+//	        // expect HTTP 200 OK, so we don't mistakenly save error report
+//	        // instead of the file
+//	        if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+//	        	throw new LinkNotAccessible("Server returned HTTP " + connection.getResponseCode() 
+//	            + " " + connection.getResponseMessage());
+////	            return "Server returned HTTP " + connection.getResponseCode() 
+////	            + " " + connection.getResponseMessage(); 
+//	        }
+	        Map<String, String> cookies_map = new HashMap();
+	        cookies_map.put("incap_ses_1220_2116053", "KTaVUOsIJnbEwWUrT1DuEJP3BGMAAAAAvhaU/6tN11Xm7F+GWi8VdA==");
+	        cookies_map.put("incap_ses_165_2116053", "dVbmAe5YASAYkV3VsjJKAsv1BGMAAAAAUmVR9ifid72ZeTElbQeu3A==");
+	        cookies_map.put("incap_ses_165_1988262", "yIU4OoOXrwiJkV3VsjJKAsv1BGMAAAAABfz6XB7fGw/lrX3iElQCHQ==");
+	        cookies_map.put("incap_ses_968_2116053", "IIVZeigRC03U3i+zhgdvDcT2BGMAAAAAoqSqU3x3jexjYrxyPkGfBQ==");
+	        cookies_map.put("nlbi_1988262_2049781", "XDJeMsEYV0NcijDal1sq3wAAAAAOm9JO9a2+cNt46gsR7EKp");
+	        cookies_map.put("nlbi_1988262", "fBnKQBVMwkWQ3lMJgBguRAAAAABScCSapleh35kmpYyck3cL");
+	        cookies_map.put("nlbi_1988262_1963038", "GXRjExWAaxoAyjWBl1sq3wAAAAAtJ/gJdAQJrskHPNyiXR/P");
+	        cookies_map.put("nlbi_1988262_1968855", "Z5bPEGDJdwfAR1Bvl1sq3wAAAAD1PBIamJ2vvBt6LZAGiKk3");
+	        cookies_map.put("nlbi_2116053", "poKlItY6KBnPUUMnF0PJmAAAAAAydDXCD6nWSFsRKFvTLcOf");
+	        cookies_map.put("nlbi_2246921", "Fgk4CRlYex5b5EzNDuMRywAAAAAPcSbAVh2C0tCmF0j6T+fL");
+	        cookies_map.put("nlbi_2208316", "Iw2rfS8GKktdFYSHzupxbQAAAABA3toTfejqFTeaTF8ybibo");
+	        cookies_map.put("nlbi_2116053_1969743", "+6qUFzkO4h6k0T9dDckrsQAAAABLVknzD+u65gQlq/54fIgS");
+	        
+	        // NEW PROBLEM TODO: Jsoup cannot access URL as we would need to be able to run Kotobee reader web app. "The Kotobee reader web application needs to be run through a web server" error.
+	        document = Jsoup.connect(url_string)
+	        		// pretend to be a human to bypass the captcha. See https://stackoverflow.com/questions/38338418/java-jsoup-html-parsing-robot-index-bot-detection-noindex
+//	        		.header("cookie", "incap_ses_1220_2116053=dVbmAe5YASAYkV3VsjJKAsv1BGMAAAAAUmVR9ifid72ZeTElbQeu3A==")
+	        		.cookies(cookies_map)
+	        		.userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.79 Safari/537.36") 
+	        		.timeout(connection_timeout_millisecond)
+	        		.get(); // IO Exception thrown // see https://jsoup.org/cookbook/input/load-document-from-url for other cool Connection modifiers to build specific requests: .data("query", "Java").userAgent("Mozilla").cookie("auth", "token").timeout(3000).post(); 
+	        // Note: Jsoup.parse is only used if you have an offline html file
+//	        document = Jsoup.parse(inputHTML.toString(), "UTF-8");
         } catch (Exception e) {
-	        return e.toString();
+        	e.printStackTrace();
         } finally {
 	        try {
 	            if (inputHTML != null)
@@ -42,35 +117,42 @@ public class PDFReader {
             if (connection != null)
             	connection.disconnect();
         	        }
-        }
+	        
+	     // See https://stackoverflow.com/questions/65959750/does-outputsettings-charset-also-change-meta-content-type for the functions below
+	        document.outputSettings().syntax(Document.OutputSettings.Syntax.xml); // specify that we want XML output // As the next step, we'll use jsoup to convert the above HTML file to a jsoup Document to render XHTML.
+//	        document.charset(StandardCharsets.UTF_8); // update the charset - also adds the <?xml encoding> instruction
+//	        document.select("meta[content~=charset]").remove(); // Remove the obsolete HTML meta tags
+	        System.out.println("\n---------------Document connected:---------------\n" + document.toString());
+        return document;
+	}
 	
-	public static Element return_Element(String href, Document document) {
-		Element element = document.head()
+	private static Element document_select_section(Document document, String html_section) {
+		Element element = document.head().selectFirst(html_section);
 		return element;
 	}
 	
-	
-	public static void main(String[] args) {
-		final String outputPdfPath = "E:/LG Documents/JavaProjects/Java_HTMLtoPDF";
-		InputStream input = null;
-		OutputStream output = null;
-        HttpURLConnection connection = null;
-        // Download the file
-        Document document = HTML_Jsoup_parse(1);
-        try {
-    		output = new FileOutputStream(outputPdfPath);
+	private static void createPdfFile(String url, String fileName) {
+		OutputStream outputStream = null;
+		try { // Note that we're wrapping our code in a try block to ensure the output stream is closed - see https://www.baeldung.com/java-html-to-pdf
+			outputStream = new FileOutputStream(fileName);
+    		ITextRenderer renderer = new ITextRenderer();
+    	    SharedContext sharedContext = renderer.getSharedContext();
+    	    sharedContext.setPrint(true);
+    	    sharedContext.setInteractive(false);
+    	    renderer.setDocument(url); // changed from renderer.setDocumentFromString(htmldoc); with input String htmldoc
+    	    renderer.layout();
+    	    renderer.createPDF(outputStream);
         } catch (Exception e) {
-	        return e.toString();
+        	e.printStackTrace();
         } finally {
 	        try {
-	            if (output != null)
-	                output.close();
+	            if (outputStream != null)
+	            	outputStream.close();
 		    } catch (IOException ignored) {
 		    }
         }
-        
-	}
-
+	    System.out.println( "PDF file: '" + fileName + "' created." );
+	    }
 }
 
 class LinkNotAccessible extends Exception { 
